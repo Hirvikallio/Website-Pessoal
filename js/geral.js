cfb6ea4bca19ddde67a9aec42c829cd60e93c08f
Hirvikallio <117479608+Hirvikallio@users.noreply.github.com>
2023-11-19 17:39:11 +0000

    kjhg

--- js/geral.js
+++ js/geral.js
@@ -6,26 +6,40 @@ document.addEventListener('mousemove', function (e) {
     document.documentElement.style.setProperty('--x', x + 'vw');
     document.documentElement.style.setProperty('--y', y + 'vh');
 });   */
+document.addEventListener('DOMContentLoaded', function () {
+    // Select elements by class 'gradient'
+    const gradients = document.querySelectorAll('.gradient');
+    const textContainer = document.querySelector('.text-container');
 
-/* gradientes background */
-// Select elements by class 'gradient'
-const gradients = document.querySelectorAll('.gradient');
+    // Add event listeners to each gradient
+    gradients.forEach((gradient) => {
+        gradient.addEventListener('mouseenter', () => {
+            gradient.style.transform = 'scale(4)';
+            // Change text color on hover
+            textContainer.style.color = getTextColor(gradient.id);
+        });
 
-const coords = { x: 0, y: 0 };
-const circles = document.querySelectorAll(".circle");
-
-const cursor = document.querySelector(".cursor");
-
-// Add event listeners to each gradient
-gradients.forEach((gradient) => {
-    gradient.addEventListener('mouseenter', () => {
-        gradient.style.transform = 'scale(4)';
+        gradient.addEventListener('mouseleave', () => {
+            gradient.style.transform = 'scale(1)';
+            // Reset text color on mouse leave
+            textContainer.style.color = '';
+        });
     });
 
-    gradient.addEventListener('mouseleave', () => {
-        gradient.style.transform = 'scale(1)';
-    });
+    // Function to get text color based on gradient ID
+    function getTextColor(gradientId) {
+        switch (gradientId) {
+            case 'hoverGradient':
+                return '#ff214f'; // Set the desired text color for this gradient
+            case 'hoverGradient1':
+                return 'rgb(1, 38, 248)'; // Set the desired text color for this gradient
+            // Add more cases for additional gradients if needed
+            default:
+                return '';
+        }
+    }
 });
+
 
 /* gradiente grande background  */
 document.addEventListener('mousemove', (event) => {
@@ -40,6 +54,6 @@ document.addEventListener('mousemove', (event) => {
         mouseXpercentage +
         '% ' +
         mouseYpercentage +
-        '%, #C3D6E5, #91F0F4, #59BAEC, #CE9CFF, #E3D4FB)';
+        '%, #d9d9d9, #8b8b8b, #444444, #000000)';
 });
 
