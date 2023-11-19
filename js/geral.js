be29cf3f76d3a4d6d84660336b83117928bf4bb5
Hirvikallio <117479608+Hirvikallio@users.noreply.github.com>
2023-11-19 15:40:04 +0000

    tyrtert

--- js/geral.js
+++ js/geral.js
@@ -1,15 +1,20 @@
-/*  gradiente */
+/*  gradiente cursor 
 document.addEventListener('mousemove', function (e) {
     let x = e.clientX / window.innerWidth * 100;
     let y = e.clientY / window.innerHeight * 100;
 
     document.documentElement.style.setProperty('--x', x + 'vw');
     document.documentElement.style.setProperty('--y', y + 'vh');
-});
+});   */
 
-
+/* gradientes background */
 // Select elements by class 'gradient'
 const gradients = document.querySelectorAll('.gradient');
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const cursor = document.querySelector(".cursor");
 
 // Add event listeners to each gradient
 gradients.forEach((gradient) => {
@@ -22,18 +27,19 @@ gradients.forEach((gradient) => {
     });
 });
 
-
+/* gradiente grande background  */
 document.addEventListener('mousemove', (event) => {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
-  
+
     mouseXpercentage = Math.round((event.pageX / windowWidth) * 100);
     mouseYpercentage = Math.round((event.pageY / windowHeight) * 100);
-  
+
     document.querySelector('body').style.background =
-      'radial-gradient(at ' +
-      mouseXpercentage +
-      '% ' +
-      mouseYpercentage +
-      '%, #3498db, #2ecc71, #e74c3c, #f39c12, #8e44ad)';
+        'radial-gradient(at ' +
+        mouseXpercentage +
+        '% ' +
+        mouseYpercentage +
+        '%, #C3D6E5, #91F0F4, #59BAEC, #CE9CFF, #E3D4FB)';
 });
+
